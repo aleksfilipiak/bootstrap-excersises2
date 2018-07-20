45ec175748e94205142ca5c8549b9d27dda4fdb4
Aleksandra Filipiak <aleksandra.filipiak12@gmail.com>
2018-07-20 09:04:16 +0200

    unnececary comments

--- js/app.js
+++ js/app.js
@@ -5,10 +5,9 @@ $(function () {
         if($(this).hasClass('active')){
 $(this).removeClass('active');
 $(this).siblings('.content').slideUp(200);
-// $(".set > a i").removeClass('fa-minus').addClass('fa-plus');
+/
         }else{
-            // $(".set> a").removeClass('fa-minus').addClass('fa-plus');
-            // $(this).find('i').removeClass('fa-plus').addClass('fa-minus');
+            
             $(".set > div").removeClass('active');
             $(this).addClass("active");
             $('content').slideUp(200);
